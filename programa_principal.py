858e262350eff264d10e45b11c0e4cd38a583b76
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-09-01 19:58:28 -0600

    Se creó la clase Estudiante que esta heredando los atributos de la clase padrre usuario. La clase Estudiante cuenta con dos atributos propios los cuales son carrera y cursos

--- programa_principal.py
+++ programa_principal.py
@@ -1,6 +1,15 @@
 class Usuario:
     def __init__(self, nombre, rol, id):
+        super().__init__()
         self.__nombre = nombre
         self.__rol = rol
         self.__id = id
+
+class Estudiante(Usuario):
+    def __init__(self, nombre, rol, id, carrera, cursos):
+        super().__init__(nombre, rol, id)
+        self.__carrera = carrera
+        self.__cursos = cursos
+
+
 
