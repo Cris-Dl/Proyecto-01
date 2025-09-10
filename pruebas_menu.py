1c472c2016cafd1148bd2351a903067011956a45
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-09-10 17:12:44 -0600

    Se creó otro archivo py para crear el menú y poder realizar futuras pruebas ya con el menú asignado.

---
+++ pruebas_menu.py
@@ -0,0 +1,48 @@
+
+
+
+
+while True:
+    print("---- Menú ----")
+    print("1. Crear Curso")
+    print("2. Administrar Curso")
+    print("3. Registrar Usuario (Estudiante/Instructor)")#Realizar sub-menú
+    print("4. Asignar Curso Estudiante")
+    print("5. Crear Tarea o Evaluación")
+    print("6. Registrar nota (Tarea/Evaluación)") #Realizar sub-menú
+    print("7. Consultar Curso")
+    print("8. Consultar Estudiantes Inscritos")
+    print("9. Consultar Tarea o Evaluación") #Realizar sub-menú
+    print("10. Consultar Calificaciones")
+    print("11. Generar reporte")
+    print("12. Salir del programa")
+    menu_option = input("Ingrese el número de la opción que quiera ingresar: ")
+    print()
+    match menu_option:
+        case "1":
+            print()
+        case "2":
+            print()
+        case "3":
+            print()
+        case "4":
+            print()
+        case "5":
+            print()
+        case "6":
+            print()
+        case "7":
+            print()
+        case "8":
+            print()
+        case "9":
+            print()
+        case "10":
+            print()
+        case "11":
+            print()
+        case "12":
+            print()
+            break
+        case _:
+            print("Valor invalido. Vuelva a intentar")
