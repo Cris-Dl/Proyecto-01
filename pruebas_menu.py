ed95ccd8422bcb680f2d83d9109b5920a7b7ab2c
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-09-11 17:20:48 -0600

    Se copio las clases de la rama estructura principal, se crreeó la clase Curso para guardar los cursos con sus respectivos métodos, se creó el metodo guardar cursos para poder guardar los cursos para el diccionario que le corresponder, se creó la clase CrearCurso con sus respectivos metodos y se creó la varaible crear_curso para acceder a la clase con el metodo correspondientee y se aplicó al menú

--- pruebas_menu.py
+++ pruebas_menu.py
@@ -1,7 +1,58 @@
+class Usuario:
+    def __init__(self, nombre, rol, id):
+        super().__init__()
+        self.__nombre = nombre
+        self.__rol = rol
+        self.__id = id
 
+class Estudiante(Usuario):
+    def __init__(self, nombre, rol, id, carrera, cursos):
+        super().__init__(nombre, rol, id)
+        self.__carrera = carrera
+        self.__cursos = cursos
 
+class Instructor(Usuario):
+    def __init__(self, nombre, rol, id, facultad, cursos):
+        super().__init__(nombre, rol, id)
+        self.__facultad = facultad
+        self.cursos = cursos
 
+class Curso:
+    def __init__(self, nombre, id):
+        self.__nombre = nombre
+        self.__id = id
 
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @nombre.setter
+    def nombre(self, nuevo_nombre):
+        if nuevo_nombre:
+            self.__nombre = nuevo_nombre
+        else:
+            print("El campo no puede estar vacio")
+
+class Informacion:
+    def __init__(self):
+        super().__init__()
+        self.cursos = {}
+
+    def guardar_cursos(self, nombre_curso, datos):
+        self.cursos[nombre_curso] = datos
+
+class CrearCurso(Informacion):
+    def __init__(self):
+        super().__init__()
+
+    def registrar_curso(self):
+        nombre_curso = input("Ingrese el nombre del curso: ")
+        id_curso = input("Ingresee el ID del curso: ")
+        datos_curso = {'nombre': nombre_curso,'ID': id_curso}
+        self.guardar_cursos(nombre_curso, datos_curso)
+        print(f"Curso '{nombre_curso}' con ID '{id_curso}' ha sido creado exitosamente.")
+
+crear_curso = CrearCurso()
 while True:
     print("---- Menú ----")
     print("1. Crear Curso")
@@ -20,6 +71,8 @@ while True:
     print()
     match menu_option:
         case "1":
+            print("Crear Curso")
+            crear_curso.registrar_curso()
             print()
         case "2":
             print()
