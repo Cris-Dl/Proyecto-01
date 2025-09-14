8d62fbcf21f13cf961c387dadd7be2ef8aefa2c0
Juan Diego <chucjuan508@gmail.com>
2025-09-14 15:58:42 -0800

    Se realizo las excepsiones de la clase ConsultarEstudiante del metodo ConsultaEstudiante, esta lanza un error cuando el input quede vacio

--- Excepciones.py
+++ Excepciones.py
@@ -696,7 +696,17 @@ class ConsultarEstudiantes:
         for id, curso in self.manejo.cursos.items():
             print(f"- ID: {id} | Nombre: {curso['nombre']}")
 
-        id_curso = input("\nIngrese el ID del curso para ver los estudiantes inscritos: ")
+        while True:
+            try:
+                id_curso = input("\nIngrese el ID del curso para ver los estudiantes inscritos: ")
+                if not id_curso.strip():
+                    raise ValueError("El id no puede quedar vacio")
+            except ValueError as e:
+                print(f"Error: {e}\n")
+            except Exception as e:
+                print(f"Error: {e}\n")
+            else:
+                break
         if id_curso not in self.manejo.cursos:
             print("Error: El curso con ese ID no existe.")
             return
